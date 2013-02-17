a828b91a7e56500363c6ad075dd38abc15ded6d2
Bulkan Evcimen <bulkan@gmail.com>
2013-02-18 10:13:07 +1100

    initial settings

---
+++ domande/settings.py
@@ -0,0 +1,13 @@
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    }
+}
+
+INSTALLED_APPS = [
+    'django.contrib.contenttypes',
+    'south',
+    'domande'
+]
+
