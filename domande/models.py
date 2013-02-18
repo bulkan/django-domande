f901578dd65664cc741d8ffc0f1da3d601cc0157
Bulkan Evcimen <bulkan@gmail.com>
2013-02-18 13:08:26 +1100

    basic model structure

--- domande/models.py
+++ domande/models.py
@@ -1,3 +1,65 @@
 from django.db import models
 
-# Create your models here.
+from django_extensions.db.models import TimeStampedModel
+
+
+class BaseQuestion(TimeStampedModel):
+    ''' represents a question '''
+
+    class Meta:
+        abstract = True
+        ordering = ['order']
+
+    order = models.PositiveIntegerField(default=1,
+        help_text='The render order')
+
+    optional = models.BooleanField(default=False,
+            help_text="If selected, user doesn't have to answer this question")
+
+    text = models.TextField(blank=False,
+        help_text='The question text')
+
+
+class TextQuestion(BaseQuestion):
+    pass
+
+
+class Choice(TimeStampedModel):
+    ''' Model to store the choices for multi answer questions'''
+
+    order = models.PositiveIntegerField(default=1)
+
+    label = models.TextField(default="")
+
+    class Meta:
+        ordering = ['order']
+
+    def __unicode__(self):
+        return self.label
+
+
+class ChoiceQuestion(BaseQuestion):
+    multichoice = models.BooleanField(default=False,
+            help_text="Select one or more")
+
+    choices = models.ForeignKey(Choice)
+
+
+class BaseAnswer(TimeStampedModel):
+    class Meta:
+        abstract = True
+
+
+class TextAnswer(BaseAnswer):
+    question =  models.ForeignKey(TextQuestion)
+
+    answer = models.TextField(blank=False,
+        help_text = 'The answer text')
+
+
+class ChoiceAnswer(BaseAnswer):
+    question =  models.ForeignKey(TextQuestion)
+
+    answer = models.ForeignKey(Choice,
+        help_text='The selected choices as the answer')
+
