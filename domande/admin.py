9990d5c616288d0d61c5fdd130d0b78583c95f37
Bulkan Evcimen <bulkan@gmail.com>
2013-02-21 14:40:39 +1100

    show answers in admin

--- domande/admin.py
+++ domande/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from models import Question, TextQuestion, ChoiceQuestion, Choice
+from models import Answer, TextAnswer, ChoiceAnswer
 
 admin.site.register(Question)
 admin.site.register(TextQuestion)
+admin.site.register(TextAnswer)
 admin.site.register(ChoiceQuestion)
+admin.site.register(ChoiceAnswer)
 admin.site.register(Choice)
