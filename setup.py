be8e777fccae594a40293d81563721f3280897fc
Bulkan Evcimen <bulkan@gmail.com>
2013-02-27 23:55:48 +1100

    added setup.py file

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+__version__ = 'dev'
+
+description = '''django app to ask questions'''
+
+setup(
+    name='django-domande',
+    packages=['domande'],
+    version=__version__,
+    description=description,
+    author='Bulkan Evcimen',
+    author_email='bulkan@gmail.com',
+    url='https://github.com/bulkan/django-domande',
+    install_requires=[
+        'django_polymporphic'
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7'
+    ]
+)
