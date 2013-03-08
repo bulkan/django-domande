ecf6f78aa0ca3511f692b4d127e872b491c08e44
Bulkan Evcimen <bulkan@gmail.com>
2013-03-08 16:36:46 +1100

    comment describing recursion problem

--- domande/models.py
+++ domande/models.py
@@ -103,4 +103,7 @@ class ChoiceAnswer(Answer):
         help_text='The selected choices as the answer')
 
     def __unicode__(self):
-        return '%s - %r' % (self.question, self.answer.all())
+        return self.question
+
+        # XXX: the following does a weird recursion depth exception o_O ?
+        #''.join(self.answer.all().values_list('label', flat=True))
